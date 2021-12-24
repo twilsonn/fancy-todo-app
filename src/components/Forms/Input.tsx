19e975adfd2c3dd483dd114acffff37f56976d0c
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-24 16:17:14 +0000

    added: form components

---
+++ src/components/Forms/Input.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Input: React.FC<{
+  title: string
+  value: string
+  changeValue: React.ChangeEventHandler<HTMLInputElement>
+}> = ({ title, value, changeValue }) => {
+  return (
+    <div className="w-full">
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700 capitalize"
+      >
+        {title.toLowerCase()}
+      </label>
+      <div className="mt-1">
+        <input
+          type="text"
+          name={title}
+          id={title}
+          value={value}
+          onChange={changeValue}
+          className="block w-full sm:text-sm rounded-md border-gray-300 shadow-sm focus:border-fuchsia-500 focus:ring-fuchsia-500"
+          autoComplete="off"
+          data-lpignore="true"
+          data-form-type="other"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Input
---
+++ src/components/Forms/Textarea.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Textarea: React.FC<{
+  title: string
+  value: string
+  changeValue: React.ChangeEventHandler<HTMLTextAreaElement>
+}> = ({ title, value, changeValue }) => {
+  return (
+    <div className="w-full">
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700 capitalize"
+      >
+        {title.toLowerCase()}
+      </label>
+      <div className="mt-1">
+        <textarea
+          name={title}
+          id={title}
+          value={value}
+          onChange={changeValue}
+          className="block w-full sm:text-sm rounded-md border-gray-300 shadow-sm focus:border-fuchsia-500 focus:ring-fuchsia-500"
+          autoComplete="off"
+          data-lpignore="true"
+          data-form-type="other"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Textarea
