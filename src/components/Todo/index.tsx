0e20676286b81e389b797f39b6df4093e854a38f
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-24 11:47:42 +0000

    update: updated to new state

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,19 +1,17 @@
+import Context from 'context'
 import React, { useContext } from 'react'
-import AppContext from 'state'
-import { v4 as uuidv4 } from 'uuid'
 
 const Todo: React.FC<unknown> = () => {
-  console.log(uuidv4())
-  const { todos } = useContext(AppContext)
+  const { state, dispatch } = useContext(Context)
 
   return (
     <div>
       <div className="flex justify-between">
-        <div>Todos: {todos?.length}</div>
+        <div>Todos: {state.todos?.length}</div>
         <div></div>
       </div>
       <div>
-        {todos?.map(({ id, title, details, tags }) => {
+        {state.todos?.map(({ id, title, details, tags }) => {
           return <div key={id}>{title}</div>
         })}
       </div>
