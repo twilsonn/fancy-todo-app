120558ca528ecd21d76c8fa164dbbc9df4436f9e
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-24 19:11:07 +0000

    added: files for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fancy-todo-app/',
   plugins: [react(), tsconfigPaths()]
 })
