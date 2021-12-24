f6e9eeb9df29e14500d0b7598c78cc8bae382128
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-24 15:04:42 +0000

    update: styled todo item

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,18 +1,34 @@
+import Container from 'components/Container'
 import Context from 'context'
 import React, { useContext } from 'react'
 
 const Todo: React.FC<unknown> = () => {
-  const { state, dispatch } = useContext(Context)
+  const { state } = useContext(Context)
 
   return (
     <div>
-      <div className="flex justify-between">
-        <div>Todos: {state.todos?.length}</div>
-        <div></div>
-      </div>
-      <div>
+      <div className="grid grid-cols-3 gap-6 mt-6">
         {state.todos?.map(({ id, title, details, tags }) => {
-          return <div key={id}>{title}</div>
+          return (
+            <Container key={id} ClassName="prose flex flex-col h-64">
+              <div>
+                <h3 className="mt-0">{title}</h3>
+                <p>{details}</p>
+              </div>
+              <div className="flex justify-end items-end space-x-2 h-full">
+                {tags?.map((tag) => {
+                  return (
+                    <span
+                      key={tag}
+                      className="py-2 px-3 text-sm font-semibold rounded-full cursor-pointer bg-slate-200 hover:bg-slate-300"
+                    >
+                      {tag}
+                    </span>
+                  )
+                })}
+              </div>
+            </Container>
+          )
         })}
       </div>
     </div>
