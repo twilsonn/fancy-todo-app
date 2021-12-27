92fcfd63399fbafc8a18d707eac2e762614bf5a3
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-27 19:56:26 +0000

    upadet: added mouse event pass through

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 
-const Container: React.FC<{ ClassName?: string }> = ({
-  children,
-  ClassName
-}) => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+const Container: React.FC<{
+  ClassName?: string
+  onClick: MouseEventHandler
+}> = ({ children, ClassName, onClick }) => {
   return (
     <div
+      onClick={onClick}
       className={`w-full p-4 bg-white shadow-md shadow-slate-200 rounded-2xl ${ClassName}`}
     >
       {children}
