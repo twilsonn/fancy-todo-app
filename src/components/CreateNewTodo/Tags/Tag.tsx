8d97b1cab3cf3f74d25250793bec6a2be98db055
Tyler Wilson <tylerwilson0729@outlook.com>
2021-12-27 19:16:44 +0000

    update: added remove tag feature

--- src/components/CreateNewTodo/Tags/Tag.tsx
+++ src/components/CreateNewTodo/Tags/Tag.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { newTodoState } from 'store'
 
 const Tag: React.FC<{ name: string }> = ({ name }) => {
+  const [todo, setTodo] = useRecoilState(newTodoState)
+
+  const removeTag = () => {
+    setTodo({ ...todo, tags: todo.tags.filter((t) => t !== name) })
+  }
+
   return (
     <span className="inline-flex items-center py-2.5 pr-2 pl-3 mt-2 mr-1 text-sm font-medium rounded-full text-slate-700 bg-slate-100">
       {name}
       <button
         type="button"
+        onClick={removeTag}
         className="inline-flex flex-shrink-0 justify-center items-center ml-0.5 w-4 h-4 focus:text-white rounded-full focus:outline-none hover:bg-slate-200 focus:bg-slate-500 text-slate-400 hover:text-slate-500"
       >
         <span className="sr-only">Remove large option</span>
