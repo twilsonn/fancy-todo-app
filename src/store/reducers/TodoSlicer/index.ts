d87594024c8290c4a529002a3138f0ba0aaacd0e
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-01 13:20:31 +0000

    update: refactored reducers

--- src/store/reducers/TodoSlicer/index.ts
+++ src/store/reducers/TodoSlicer/index.ts
@@ -1,109 +1,64 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { v4 } from 'uuid'
+import { CaseReducer, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import type { RootState } from '../../index'
+import initialState, { Todo, TodoState } from './types'
 
-type Todo = {
-  id: string
-  title: string
-  details: string
-  tags: string[]
-  active: boolean
+const addTodoAction: CaseReducer<TodoState, PayloadAction<Todo>> = (
+  state,
+  action
+) => {
+  return { ...state, value: [...state.value, action.payload] }
 }
 
-interface TodoState {
-  value: Todo[]
-  filter: string
+const deleteTodoAction: CaseReducer<TodoState, PayloadAction<string[]>> = (
+  state,
+  action
+) => {
+  return {
+    ...state,
+    value: [...state.value.filter((t) => !action.payload.includes(t.id))]
+  }
 }
 
-const initialState: TodoState = {
-  value: [
-    {
-      id: v4(),
-      title: 'Water my plants 💦',
-      details: "The cactus' and snake plant needs watering on sunday morning!",
-      tags: ['plants', 'house'],
-      active: true
-    },
-    {
-      id: v4(),
-      title: 'Pick up package from store',
-      details: 'The store is open until 6pm',
-      tags: ['travel', 'shopping'],
-      active: true
-    },
-    {
-      id: v4(),
-      title: 'Order some new headphones 🎶',
-      details: 'Look online for open ear headphones!',
-      tags: ['shopping', 'online'],
-      active: true
-    },
-    {
-      id: v4(),
-      title: 'Play Elder scrolls online 🤺',
-      details: "don't take an arrow to the knee",
-      tags: ['online', 'gaming'],
-      active: true
-    },
-    {
-      id: v4(),
-      title: 'Contribute to open source development!',
-      details:
-        'Try to get involved in open source dev. Start with the small tasks.',
-      tags: ['online', 'coding'],
-      active: true
+const filterTodosAction: CaseReducer<TodoState, PayloadAction<string>> = (
+  state,
+  action
+) => {
+  if (state.filter === action.payload) {
+    return {
+      ...state,
+      filter: '',
+      value: [
+        ...state.value.map((t) => {
+          return { ...t, active: true }
+        })
+      ]
     }
-  ],
-  filter: ''
+  }
+
+  return {
+    ...state,
+    filter: action.payload,
+    value: [
+      ...state.value.map((t) =>
+        t.tags.includes(action.payload) ? t : { ...t, active: false }
+      )
+    ]
+  }
 }
 
 export const TodoSlice = createSlice({
   name: 'todos',
   initialState,
   reducers: {
-    addTodo: (state, action: PayloadAction<Todo>) => {
-      state.value = [...state.value, action.payload]
-    },
-    deleteTodo: (state, action: PayloadAction<string[]>) => {
-      state.value = [
-        ...state.value.filter((t) => !action.payload.includes(t.id))
-      ]
-    },
-    filterTodos: (state, action: PayloadAction<string>) => {
-      if (state.filter === action.payload) {
-        state.value = [
-          ...state.value.map((t) => {
-            return { ...t, active: true }
-          })
-        ]
-        state.filter = ''
-        return
-      }
-      state.filter = action.payload
-      state.value = [
-        ...state.value.map((t) =>
-          t.tags.includes(action.payload) ? t : { ...t, active: false }
-        )
-      ]
-    }
+    addTodo: addTodoAction,
+    deleteTodo: deleteTodoAction,
+    filterTodos: filterTodosAction
   }
 })
 
-export const blankTodo: () => Todo = () => {
-  return {
-    id: v4(),
-    title: '',
-    details: '',
-    tags: [],
-    active: true
-  }
-}
-
-export type { Todo }
-
 export const { addTodo, deleteTodo, filterTodos } = TodoSlice.actions
 
-export const selectTodo = (state: RootState) => state.todos.value
+export const selectTodo = (state: RootState) => state.todos.present.value
 
 export default TodoSlice.reducer
