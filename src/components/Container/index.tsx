55f0965ace751361351da97c17519cb92145ba11
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-01 13:18:44 +0000

    update: added dataValue attribute

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,11 +3,13 @@ import React, { MouseEventHandler } from 'react'
 // eslint-disable-next-line @typescript-eslint/ban-types
 const Container: React.FC<{
   ClassName?: string
+  dataValue?: string
   onClick?: MouseEventHandler
-}> = ({ children, ClassName, onClick }) => {
+}> = ({ children, ClassName, dataValue, onClick }) => {
   return (
     <div
       onClick={onClick}
+      data-value={dataValue}
       className={`w-full p-4 bg-white shadow-md shadow-slate-200 rounded-2xl ${ClassName}`}
     >
       {children}
