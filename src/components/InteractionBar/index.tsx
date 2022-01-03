89a0afc6c5f9d35b02566dca4a3b5a336045732a
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-03 18:23:41 +0000

    update: added animate height grow

--- src/components/InteractionBar/index.tsx
+++ src/components/InteractionBar/index.tsx
@@ -1,20 +1,21 @@
-import { useAppSelector } from 'hooks'
 import React from 'react'
+import { useAppSelector } from 'hooks'
+import { motion } from 'framer-motion'
 
 const InteractionBar: React.FC = () => {
   const todos = useAppSelector((state) => state.todos.present)
-  return (
-    <div className="flex justify-between">
-      <div># of Todos: {todos.value.length}</div>
-      <div>
-        {todos.filter && (
-          <span className="inline-flex items-center py-2.5 px-3 mt-2 mr-1 text-sm font-medium rounded-full text-slate-700 bg-slate-200">
-            {todos.filter}
-          </span>
-        )}
-      </div>
-    </div>
-  )
+  return todos.filter ? (
+    <motion.div
+      initial={{ height: '0', opacity: 0 }}
+      animate={{ height: 'auto', opacity: '1' }}
+      className="flex items-center h-10"
+    >
+      <span className="mr-3">Filters: </span>
+      <span className="inline-flex items-center py-2.5 px-3 mr-1 text-sm font-medium rounded-full text-slate-700 bg-slate-200">
+        {todos.filter}
+      </span>
+    </motion.div>
+  ) : null
 }
 
 export default InteractionBar
