9f6f82a2c56a5a4031191b00dc01207c104bd39e
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-04 22:55:03 +0000

    update: added react-grid-layout

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, MouseEvent, KeyboardEvent } from 'react'
+import GridLayout from "react-grid-layout";
 import useKeypress from 'react-use-keypress'
 
 import { useAppDispatch, useAppSelector, useEventListener } from '../../hooks'
@@ -12,6 +13,12 @@ import { ActionCreators } from 'redux-undo'
 const TodoList: React.FC = () => {
   const todos = useAppSelector((state) => state.todos.present.value)
   const dispatch: AppDispatch = useAppDispatch()
+
+  const layout = todos.map((t, i) => {
+    return {
+      i: t.id, x: i % 3, y: (i % 2) , w: 1, h: 1
+    }
+  })
 
   const [selected, setSelected] = useState<Todo[] | []>([])
 
@@ -52,46 +59,55 @@ const TodoList: React.FC = () => {
   })
 
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-6 mt-3">
-        {todos.map((todo) => {
-          const { id, title, details, tags } = todo
-          return todo.active ? (
-            <Container
-              key={id}
-              onClick={(e) => toggleSelected(e, todo)}
-              dataValue={
-                selected.some((t) => t.id === id) ? 'todo selected' : 'todo'
-              }
-              ClassName={`prose flex flex-col h-64 cursor-pointer select-none ${
-                selected.some((t) => t.id === id)
-                  ? 'ring-fuchsia-500 ring-2'
-                  : ''
-              }`}
-            >
-              <div className="pointer-events-none">
-                <h3 className="mt-0">{title}</h3>
-                <p>{details}</p>
-              </div>
-              <div className="flex justify-end items-end space-x-2 h-full">
-                {tags.map((tag) => {
-                  return (
-                    <span
-                      key={tag}
-                      data-value="tag"
-                      onClick={() => dispatch(filterTodos(tag))}
-                      className="py-2 px-3 text-sm font-semibold rounded-full cursor-pointer bg-slate-200 hover:bg-slate-300"
-                    >
-                      {tag}
-                    </span>
-                  )
-                })}
-              </div>
-            </Container>
-          ) : null
-        })}
-      </div>
-    </div>
+    <GridLayout 
+      className="layout mt-3"
+      containerPadding={[0,0]}
+      layout={layout} 
+      cols={3}
+      rowHeight={255}
+      width={900}
+      compactType="horizontal"
+    >
+      {todos.map((todo) => {
+        const { id, title, details, tags } = todo
+        return todo.active ? (
+          <div key={id}>
+
+          <Container
+            key={id}
+            onClick={(e) => toggleSelected(e, todo)}
+            dataValue={
+              selected.some((t) => t.id === id) ? 'todo selected' : 'todo'
+            }
+            ClassName={`prose flex flex-col h-64 cursor-pointer select-none ${
+              selected.some((t) => t.id === id)
+                ? 'ring-fuchsia-500 ring-2'
+                : ''
+            }`}
+          >
+            <div className="pointer-events-none">
+              <h3 className="mt-0">{title}</h3>
+              <p>{details}</p>
+            </div>
+            <div className="flex justify-end items-end space-x-2 h-full">
+              {tags.map((tag) => {
+                return (
+                  <span
+                    key={tag}
+                    data-value="tag"
+                    onClick={() => dispatch(filterTodos(tag))}
+                    className="py-2 px-3 text-sm font-semibold rounded-full cursor-pointer bg-slate-200 hover:bg-slate-300"
+                  >
+                    {tag}
+                  </span>
+                )
+              })}
+            </div>
+          </Container>
+          </div>
+        ) : null
+      })}
+    </GridLayout>
   )
 }
 
