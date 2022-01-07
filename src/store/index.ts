e943a274925fd71bf627afc59e6e8e7b8048667d
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-07 20:13:43 +0000

    update: added redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createStore } from '@reduxjs/toolkit'
 
 import todosReducer from './reducers/TodoSlicer'
 import undoable from 'redux-undo'
 
-// const filter = get(todoListFilterState)
-// const list = get(todoListState)
-// return filter
-//   ? list.filter((t) => t.tags.includes(filter) && t.active)
-//   : list.filter((t) => t.active)
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({
-  reducer: {
-    todos: undoable(todosReducer)
-  }
-})
+const persistConfig = {
+  key: 'todos',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, undoable(todosReducer))
+
+const store = createStore(persistedReducer)
+
+const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
-export default store
+export default () => {
+  return {store, persistor}
+}
