4d56095b553153ea31b22436ecd3721f6eb45d4e
Tyler Wilson <tylerwilson0729@outlook.com>
2022-01-07 20:14:03 +0000

    update: added redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import NewStore from './store'
 
 import '/node_modules/react-grid-layout/css/styles.css'
 import '/node_modules/react-resizable/css/styles.css'
@@ -9,12 +9,17 @@ import '/node_modules/react-resizable/css/styles.css'
 import './assets/index.css'
 
 import App from 'components/App'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const {store, persistor} = NewStore()
 
 const Main = () => {
   return (
     <StrictMode>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
       </Provider>
     </StrictMode>
   )
